135cde3a49762baea35001f2c8c1ba2ac9302e40
liyyf <liyyf@tuya.com>
2022-11-09 16:59:44 +0800

    fix: fix bug

--- src/data/skillGain/fenjiaoshi.ts
+++ src/data/skillGain/fenjiaoshi.ts
@@ -8,7 +8,7 @@ const fenjiaoGainDTO: SkillGainDTO[] = [
   {
     增益名称: '中峙',
     增益所在位置: '奇穴',
-    常驻增益: true,
+    常驻增益: false,
     增益集合: [
       {
         增益类型: GainTypeEnum.伤害百分比,
@@ -20,7 +20,7 @@ const fenjiaoGainDTO: SkillGainDTO[] = [
   {
     增益名称: '渊冲',
     增益所在位置: '奇穴',
-    常驻增益: false,
+    常驻增益: true,
     增益集合: [
       {
         增益类型: GainTypeEnum.外攻会心百分比,
