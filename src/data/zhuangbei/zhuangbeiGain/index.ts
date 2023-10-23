74e347168976f3da7e9edad104bc12ad2e82e42c
liyyf <liyyf@tuya.com>
2023-10-23 15:59:32 +0800

    fix: 增益

--- src/data/zhuangbei/zhuangbeiGain/index.ts
+++ src/data/zhuangbei/zhuangbeiGain/index.ts
@@ -103,14 +103,16 @@ const ZhuangbeiGainList = {
     {
       增益计算类型: GainDpsTypeEnum.A,
       增益类型: GainTypeEnum.外攻破防等级,
-      增益数值: 999,
+      // 增益数值: 999,
+      增益数值: 1100,
     },
   ],
   大附魔_伤衣: [
     {
       增益计算类型: GainDpsTypeEnum.A,
       增益类型: GainTypeEnum.基础攻击,
-      增益数值: 450,
+      // 增益数值: 450,
+      增益数值: 500,
     },
   ],
   大附魔_伤腰: [
