95cf97daedfef33935b795b5dd8c5361b6a3f2d9
liyyf <liyyf@tuya.com>
2023-11-17 23:19:02 +0800

    fix: 更新阵眼无双bug

--- src/utils/dps-kernel-optimizer/dps-kernel-optimizer.ts
+++ src/utils/dps-kernel-optimizer/dps-kernel-optimizer.ts
@@ -59,7 +59,7 @@ const DpsKernelOptimizer = ({
     })
 
     // 由于dps太大，导致用1除灰远小于计算容差，所以这里取一个较大的值去除以最终结果。以达到dps越大最终结果越小的目的，用于算法计算
-    return 100000 / totalDps
+    return 100000000 / totalDps
   }
 
   const maxObj = optimizationTool({ getDpsFunction, initialGuess: [0.3, 0.8], tol: 1e-12 })
